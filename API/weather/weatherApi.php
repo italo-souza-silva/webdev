1fc09a0c981083843de08f64fbd1d1330403e282
ItaloSSilva19 <iti.s.silva@gmail.com>
2020-09-01 18:03:13 -0300

    Finished the weather web page

--- API/weather/weatherApi.php
+++ API/weather/weatherApi.php
@@ -1,10 +1,10 @@
 <?php
-	
+	include ("APIpasswordKey.php");
 	$weather = "";
 	$error = "";
 	
 	if ($_GET["city"]) {
-		$urlContents = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".urlencode($_GET["city"])."&appid=e44bd71850adbc5315a25c2ec1df8244");
+		$urlContents = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".urlencode($_GET["city"])."&appid=".$password);
 		$weatherArray = json_decode($urlContents, true);
 		if ($weatherArray["cod"] == 200) {
 			$weather = "The weather in ".$_GET["city"]." is currently '".$weatherArray["weather"][0]["description"]."'.<br /> ";
