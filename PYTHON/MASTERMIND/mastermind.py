9ba04c30dd50a2ca83b329e473aa7c4e6ce01587
ItaloSSilva19 <iti.s.silva@gmail.com>
2020-09-14 17:59:20 -0300

    create a form

--- PYTHON/MASTERMIND/mastermind.py
+++ PYTHON/MASTERMIND/mastermind.py
@@ -5,3 +5,12 @@ print ('')
 
 import cgi
 form = cgi.FieldStorage()
+
+print ('<h1>Mastermind</h1>')
+print ("<p>" + message + "</p>")
+print ('<form method="post">')
+print ('<input type="text" name="guess" value="' + guess + '">')
+print ('<input type="hidden" name="answer" value = "' + answer + '">')
+print ('<input type="hidden" name="numberOfGuesses" value = "' + str(numberOfGuesses) + '">')
+print ('<input type="submit" value="Guess!">')
+print ('</form>')
