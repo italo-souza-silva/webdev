fc7268a810b0ca4605ecebf3771414d53bc485cb
ItaloSSilva19 <iti.s.silva@gmail.com>
2020-09-23 17:26:38 -0300

    Removed MySQL folder

--- MYSQL/Diary/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$link = mysqli_connect("shareddb-v.hosting.stackcp.net","diary-DB-313437cdb6","313437cdb6","diary-DB-313437cdb6"); //this will be changed of course!
-		if (mysqli_connect_error()) {
-			die ("Database Connection Error");
-		}
-?>
